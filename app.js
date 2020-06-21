933cde5863f84a8b9cf7e2f1e09ebf000e5502ff
Meskerem Ayale <55857642+memeayale@users.noreply.github.com>
2020-06-20 19:08:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ var logger = require('morgan');
 
 let credentials = require('./credentials');//store mongodb credentials in separate, non-tracked file
 var db_admin = credentials.getCredentials();
+console.log(db_admin);
 
-//now using monk to handle MongoDB
+//monk will be our db connection tool
 var monk = require('monk');
+//connection to Atlas
 var uri = "mongodb+srv://" + db_admin.username + ":" + db_admin.password + "@cluster0-i3nnd.gcp.mongodb.net/test_db?retryWrites=true&w=majority";
-// Connect to the db
 var db = monk(uri);
 
-db.then(() => {
-  console.log('Connected correctly to server');
+db.then(()=>{
+  console.log('Connected to server');
 });
-
+/*
 const collection = db.get('test_collection');
-
 collection.find({}, function(err, docs){
     if(err){
       console.log(err);
@@ -26,20 +26,20 @@ collection.find({}, function(err, docs){
 	console.log(docs);
     }
 });
-
-/*
-collection.insert([{a: 1}, {a: 2}, {a: 3}])
-  .then((docs) => {
-    // Inserted 3 documents into the document collection
-	console.log(docs);
-  })
-  */
+*/
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.get('/collections/:name',function(req,res){
+  var collection = db.get(req.params.name);
+  collection.find({},{limit:20},function(e,docs){
+    res.json(docs);
+  })
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,13 +59,6 @@ app.use(function(req,res,next){
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.get('/collections/:name',function(req,res){
-  var collection = db.get(req.params.name);
-  collection.find({},{limit:20},function(e,docs){
-    res.json(docs);
-  })
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
